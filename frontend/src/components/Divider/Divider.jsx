a6f781fc7e3d9adf1d4d799fa9e402131d4d196b
ali-hbl <nwah.ally@gmail.com>
2023-07-06 13:39:28 +0200

    clean code

--- frontend/src/components/Divider/Divider.jsx
+++ frontend/src/components/Divider/Divider.jsx
@@ -2,20 +2,7 @@ import './styles.scss';
 
 const Divider = () => {
   return (
-    <div className="divider">
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-      <span className="circle"></span>
-    </div>
+    <div className="divider"><span></span></div>
   );
 };
 
