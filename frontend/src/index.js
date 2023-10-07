61cc0176452009d0cb3413d16569ead6024d3a4b
ali-hbl <nwah.ally@gmail.com>
2023-10-07 15:48:07 +0200

    try again

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { UserContextProvider } from '../src/context/UserContext';
+import { UserContextProvider } from '../src/context/UserContext.jsx';
 import CartProvider from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
