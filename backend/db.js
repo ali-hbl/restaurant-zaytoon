4b235cccc1627bba501035d4d408b9c88df4686e
ali-hbl <nwah.ally@gmail.com>
2023-10-09 11:16:27 +0200

    init render hosting and clean backend files

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv').config();
+
+// Connection to DB
+const connection = mysql.createConnection({
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: 8889,
+});
+
+module.exports = connection;
