734f787badbc3379c0b83047bfdd17bfdd2eeb9a
ali-hbl <nwah.ally@gmail.com>
2025-07-28 22:17:31 +0200

    trying to deploy railway

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,16 @@ const dotenv = require('dotenv').config();
 
 // Connection to DB
 const connection = mysql.createConnection({
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  // database: process.env.DB_DATABASE,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 module.exports = connection;
